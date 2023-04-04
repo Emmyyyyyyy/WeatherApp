9d7621dff033c65111fbf7c974233100e6ed5da6
Emmyyyyyyy <eemmyy.a@gmail.com>
2023-04-04 23:55:02 +0700

    Update api

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable no-template-curly-in-string */
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import { Header } from "./components/header";
 import { Lang } from "./components/Lang";
 import { SearchBar } from "./components/searchBar";
 import { Info } from "./components/info";
 import { MoreInfoButton } from "./components/moreInfoButton";
-import weatherPic from "./static/images/02d@2x.png";
 
 import "./App.css"
 
 
 export default function App() {
   const [flip, setFlip] = useState(false)
-  const [storeData, setStoreData] = useState({})
-
-  useEffect(()=>{
-    const fetchData = async () => {
-      const weatherData = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=33e89e9f5985c4a59d8060e8f122b223&units=metric", {
-        method: "GET"
-      });
-      const weatherInfo = await weatherData.json();
-      setStoreData(weatherInfo)
-    }
-    const intervalId = setInterval(fetchData, 1000);
-    return () => clearInterval(intervalId);
-    // fetchData();
-  }, [])
-
-  const weather = storeData?.weather?.[0]?.description
-  console.log(weather?.[0]?.description)
 
   return (
     <>
@@ -41,8 +23,7 @@ export default function App() {
           <div className="center-context">
             <Header />
             <SearchBar />
-            <img src={weatherPic} className="weather-pic" />
-            <Info city={storeData?.name} temp={storeData?.main?.temp} desc={storeData?.weather?.[0]?.description}/>
+            <Info />
           </div>
         </div>
         <div className="back-card">
@@ -51,7 +32,6 @@ export default function App() {
           <div className="center-context">
             <Header />
             <SearchBar />
-            <img src={weatherPic} className="weather-pic" />
             <Info />
           </div>
         </div>
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,43 +1,60 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Dropdown } from "react-bootstrap";
 import chevron from "../static/images/down-chevron.png"
+import weatherPic from "../static/images/02d@2x.png";
+
 
 export const Info = (props) => {
     const [selected, setSelected] = useState(false)
-    const [selectedUnit, setSelectedUnit] = useState("EN");
+    const [selectedUnit, setSelectedUnit] = useState("°C");
+    const [storeData, setStoreData] = useState({})
+
+    useEffect(()=>{
+        const fetchData = async () => {
+        const weatherData = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=33e89e9f5985c4a59d8060e8f122b223&units=metric", {
+            method: "GET"
+        });
+        const weatherInfo = await weatherData.json();
+        setStoreData(weatherInfo)
+        }
+        const intervalId = setInterval(fetchData, 1000);
+        return () => clearInterval(intervalId);
+        // fetchData();
+    }, [])
 
     const handlerSelect = (eventKey, event) => {
         setSelectedUnit(eventKey);
     }
 
-    var roundNum = Math.round((props.temp) * 10) / 10;
+    var roundNum = Math.round((storeData?.main?.temp) * 10) / 10;
     console.log(roundNum);
 
     return (
         <>
-            <p className="country">{props.city}</p>
+            <img src={weatherPic} className="weather-pic" />
+            <p className="country">{storeData?.name}</p>
             <div className='temp-box'>
-                <p className="temp">{roundNum} C</p>
+                <p className="temp">{roundNum} {selectedUnit}</p>
                 <Dropdown className="unit-btn-box" onClick={() => setSelected(!selected)}>
                 <Dropdown.Toggle variant="success" id="dropdown-basic" className="unit-btn">
                     <img src={chevron} className={selected ? "chevron chevron-selected" : "chevron"}/>
                 </Dropdown.Toggle>
                 <Dropdown.Menu className="unit-menu">
-                    <Dropdown.Item className="unit-item" eventKey="EN" active={selectedUnit === "EN"} onClick={(event) => handlerSelect("EN", event)}>
-                        Kelvin
-                    </Dropdown.Item>
-                    <Dropdown.Item className="unit-item" eventKey="JP" active={selectedUnit === "JP"} onClick={(event) => handlerSelect("JP", event)}>
+                    <Dropdown.Item className="unit-item" eventKey="°C" active={selectedUnit === "°C"} onClick={(event) => handlerSelect("°C", event)}>
                         Celsius
                     </Dropdown.Item>
-                    <Dropdown.Item className="unit-item" eventKey="ES" active={selectedUnit === "ES"} onClick={(event) => handlerSelect("ES", event)}>
+                    <Dropdown.Item className="unit-item" eventKey="K" active={selectedUnit === "K"} onClick={(event) => handlerSelect("K", event)}>
+                        Kelvin
+                    </Dropdown.Item>
+                    <Dropdown.Item className="unit-item" eventKey="°F" active={selectedUnit === "°F"} onClick={(event) => handlerSelect("°F", event)}>
                         Fahrenheit
                     </Dropdown.Item>
                 </Dropdown.Menu>
                 </Dropdown>
             </div>
             
-            <p>{props.desc}</p>
+            <p>{storeData?.weather?.[0]?.description}</p>
         </>
     )
 }
