134a93496916f83a8ef7d7d1bb47b635afcd2cdd
Emmy <eemmyy.a@gmail.com>
2023-05-04 12:45:01 +0700

    Edit pm2.5 data link

--- src/services/pm2.5API.js
+++ src/services/pm2.5API.js
@@ -1,5 +1,5 @@
 export async function fetchPMData(setStoreData) {
-    const pmData = await fetch("http://api.airvisual.com/v2/nearest_city?key=654948dd-af50-4598-b735-ef036f01b063", {
+    const pmData = await fetch("https://api.airvisual.com/v2/nearest_city?key=654948dd-af50-4598-b735-ef036f01b063", {
       method: "GET"
     });
     const pmInfo = await pmData.json();
